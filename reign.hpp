606dcdc17b67afe4a5b3e0cbfc4703594f74e9b4
Malachi Francesco Acri <112911114+Paxsma@users.noreply.github.com>
2024-11-22 06:36:40 -0500

    Add files via upload

---
+++ reign.hpp
@@ -0,0 +1,81 @@
+#include <array>
+#include <cstdint>
+
+namespace reign {
+
+      template <typename T, std::size_t N>
+      class fixed_vector {
+
+          public:
+
+            fixed_vector()
+                : size_(0) {
+            }
+
+            constexpr std::size_t max_size() const {
+                  return N;
+            }
+
+            inline std::size_t size() const {
+                  return size_;
+            }
+            inline bool empty() const {
+                  return !this->size();
+            }
+            inline void push_back(const T &value) {
+                  if (this->size_ >= N) {
+                        throw std::out_of_range("Overflow");
+                  }
+                  this->data[this->size_++] = value;
+            }
+            inline void push_back(T &&value) {
+                  if (this->size_ >= N) {
+                        throw std::out_of_range("Overflow");
+                  }
+                  this->data[this->size_++] = std::move(value);
+            }
+            inline void pop_back() {
+                  if (!this->size_) {
+                        throw std::out_of_range("Underflow");
+                  }
+                  --this->size_;
+            }
+            T &at(const std::size_t idx) {
+                  if (idx >= this->size_) {
+                        throw std::out_of_range("Index out of range");
+                  }
+                  return this->data[idx];
+            }
+            const T &at(const std::size_t idx) const {
+                  if (idx >= size_) {
+                        throw std::out_of_range("Index out of range");
+                  }
+                  return this->data[idx];
+            }
+
+            T &operator[](const std::size_t idx) {
+                  return this->data[idx];
+            }
+            const T &operator[](const std::size_t idx) const {
+                  return this->data[idx];
+            }
+
+            auto begin() {
+                  return this->data.begin();
+            }
+            auto end() {
+                  return this->data.begin() + this->size_;
+            }
+            auto begin() const {
+                  return this->data.begin();
+            }
+            auto end() const {
+                  return this->data.begin() + this->size_;
+            }
+
+         private:
+           std::array<T, N> data;
+           std::size_t size_ = 0u;
+      };
+
+} // namespace reign
